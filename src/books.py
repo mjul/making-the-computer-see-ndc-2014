184940de5399c8548250f0da9c249561bc71c475
Martin Jul <martin@mjul.com>
2014-05-24 17:01:57 +0200

    Updated page-finding and binarization code.

--- src/books.py
+++ src/books.py
@@ -12,10 +12,12 @@ import cv2
 from tesserwrap import Tesseract
 import PIL as pil
 
-
-
 import re
 import os
+
+# ----------------------------------------------------------------
+
+SAVE_SCREENSHOTS = False
 
 # ----------------------------------------------------------------
 
@@ -68,17 +70,22 @@ def find_quadrilaterals(img, min_area, cos_epsilon):
                 quads.append(cnt)
     return quads
 
+# ----------------------------------------------------------------
 
 def show(title, img):
+    '''Show the image.
+       As a side-effect saves the image for use in the presentation.'''
     cv2.imshow(title, img)
     fname = "../screenshots/books %s.jpg" % title.replace(':', '')
-    print "Writing %s..." % fname
-    cv2.imwrite(fname, img)
+    if SAVE_SCREENSHOTS:
+        print "Writing %s..." % fname
+        cv2.imwrite(fname, img)
     
-    
+# ----------------------------------------------------------------
 
 def find_page_with_canny(img):
-    '''Find the page in the image.'''
+    '''Find the page in the image.
+       Returns the largest rectangular contour found.'''
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     show('1: Input image', gray)
     # Remove high-frequency noise (e.g. letters)
@@ -92,16 +99,64 @@ def find_page_with_canny(img):
     quads = find_quadrilaterals(dilated, min_area=min_page_area, cos_epsilon=.8)
     quads_found = draw_contours(img, quads)
     show('5: Page found', quads_found)
-    
+    largest = sorted(quads, key=lambda q: cv2.contourArea(q))[-1]
+    return largest
 
+
+def create_mask(img, cnt):
+    '''Create a mask of the same size as the image
+       based on the interior of the contour.'''
+    mask = np.zeros((img.shape[0], img.shape[1]), np.uint8)
+    cv2.drawContours(mask, [cnt], 0, 255, -1)
+    return mask
+
+
+def binarize_adaptive(img, mask):
+    '''Convert an image to binary form using adaptive thresholding,
+       restricting to only the mask area (where the mask is non-zero).'''
+    gray_full = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_masked = cv2.bitwise_and(gray_full, gray, mask=mask)
+    
+    binarized = cv2.adaptiveThreshold(gray_masked, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,
+                                      blockSize=3, C=5)
+    return binarized
+
+ 
+# ----------------------------------------------------------------
 
 if  __name__ =='__main__':
     raw = cv2.imread('../images/books/Microserfs_p87_2.jpg')
-    blurred = cv2.GaussianBlur(raw, (3,3), 0)
-    small = cv2.resize(blurred, (480,640))
 
-    #find_page_with_morphology(small)
-    find_page_with_canny(small)
+    SAVE_SCREENSHOTS = True
+    scale_factor = raw.shape[0] / 640    # .shape is (height, width, depth)
+    print raw.shape
     
+    # Work on a small image for speed and better screen-size fit while developing
+    blurred = cv2.GaussianBlur(raw, (3,3), 0)
+    small = cv2.resize(blurred, (480,640)) # size is (width,height)
+    
+    #find_page_with_morphology(small)
+    page_contour_small = find_page_with_canny(small)
+
+    # Go back to full resolution to get full fidelity text for OCR
+    page_contour = scale_factor * page_contour_small
+    page_mask = create_mask(raw, page_contour)
+    # Get the interior of the page, not the edges
+    # (erode grows the black, non-page area)
+
+    # This includes the page border
+    binarized = binarize_adaptive(raw, page_mask)
+    
+    # Make the non-interior part of the page white
+    interior_mask = cv2.erode(page_mask, kernel=None, iterations=40)
+    not_interior = cv2.bitwise_not(interior_mask)
+    clipped_binarized = cv2.max(binarized, not_interior)
+    gray = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)
+    clipped = cv2.max(gray, not_interior)
+
+    show('6: Binarized', clipped_binarized)
+    
+    print "Press any key..."
     cv2.waitKey()
     cv2.destroyAllWindows()
+    print "Done."
