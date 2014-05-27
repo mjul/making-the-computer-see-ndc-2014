194cf21e02a5cf695694a4ad9e52f5c8fd34166f
Martin Jul <martin@mjul.com>
2014-05-27 21:15:49 +0200

    More readable find quadrilaterals code

--- src/books.py
+++ src/books.py
@@ -59,15 +59,16 @@ def angle_cos(p0, p1, p2):
 
 def find_quadrilaterals(img, min_area, cos_epsilon):
     quads = []
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # findContours mutates the image, work on a copy
+    contours, hierarchy = cv2.findContours(img.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
         cnt_len = cv2.arcLength(cnt, True)
-        cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
-        if len(cnt) == 4 and cv2.contourArea(cnt) > min_area and cv2.isContourConvex(cnt):
-            cnt = cnt.reshape(-1, 2)
-            max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
+        poly = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
+        if len(poly) == 4 and cv2.contourArea(poly) > min_area and cv2.isContourConvex(poly):
+            poly = poly.reshape(-1, 2)
+            max_cos = np.max([angle_cos( poly[i], poly[(i+1) % 4], poly[(i+2) % 4] ) for i in xrange(4)])
             if max_cos < cos_epsilon:
-                quads.append(cnt)
+                quads.append(poly)
     return quads
 
 # ----------------------------------------------------------------
