3c11adac22e9fbb8bc036ad52d3ebd6eb8adc9bf
Martin Jul <martin@mjul.com>
2014-06-01 16:33:58 +0200

    Pizza box delivery spotting completed.

--- src/pizza.py
+++ src/pizza.py
@@ -11,6 +11,47 @@ import cv2
 # Histogram and plotting
 import matplotlib.pyplot as plt
 
+# ----------------------------------------------------------------
+
+SAVE_SCREENSHOTS = False
+
+# ----------------------------------------------------------------
+
+def screenshot_filename(title):
+    return "../screenshots/pizza %s.jpg" % title.replace(':', '')
+
+def save_image(title, img):    
+    fname = screenshot_filename(title)
+    print "Writing %s..." % fname
+    cv2.imwrite(fname, img)
+
+def show(title, img):
+    '''Display the image.
+       As a side-effect, save it if SAVE_SCREENSHOTS is True.'''
+    h,w = img.shape[:2]
+    if (h > 480) or (w >1024):
+        wh_ratio = 1.0 * w/h
+        if (h < w):
+            small_shape = (640, 640*h/w)
+        else:
+            small_shape = (480, 480*h/w)
+        small = cv2.resize(img, small_shape)
+    else:
+        small = img
+    cv2.imshow(title, small)
+    if SAVE_SCREENSHOTS:
+        save_image(title, img)
+
+# ----------------------------------------------------------------
+
+def read_scene(fname, image_type):
+    '''Read a door scene image and crop it to the interior of the door frame.'''
+    whole_scene = cv2.imread(fname, image_type)
+    assert whole_scene != None, "Could not load %s" % fname
+    upright = cv2.flip(cv2.transpose(whole_scene), 0)
+    x,y,w,h = 610,900,3*440,4*440
+    cropped = upright[y: y + h, x: x + w]
+    return cropped
 
 # ----------------------------------------------------------------
 
@@ -20,7 +61,7 @@ def draw_keypoints(img, keypoints, colour = (0, 255, 255)):
     for kp in keypoints:
             x, y = kp.pt
             r = kp.size
-            cv2.circle(result, (int(x), int(y)), int(r), colour, thickness=10)
+            cv2.circle(result, (int(x), int(y)), int(r), colour, thickness=2)
     return result
 
 # ----------------------------------------------------------------
@@ -39,26 +80,10 @@ def filter_matches(kp1, kp2, matches, ratio = 0.75):
     kp_pairs = zip(mkp1, mkp2)
     return p1, p2, kp_pairs
 
-# ----------------------------------------------------------------
 
-def show(title, img):
-    small_shape = (480,640) if (img.shape[0] > img.shape[1]) else (640,480)
-    small = cv2.resize(img, small_shape)
-    cv2.imshow(title, small)
-    
-# ----------------------------------------------------------------
-
-def read_scene(fname, image_type):
-    '''Read a door scene image and crop it to the interior of the door frame.'''
-    whole_scene = cv2.imread(fname, image_type)
-    upright = cv2.flip(cv2.transpose(whole_scene), 0)
-    x,y,w,h = 610,900,3*440,4*440
-    cropped = upright[y: y + h, x: x + w]
-    return cropped
-
-# ----------------------------------------------------------------
-
-def explore_match(win, img1, img2, kp_pairs, status = None, H = None):
+def draw_matches(img1, img2, kp_pairs, status = None, H = None):
+    assert img1.ndim == 2, "Expected single-channel img1."
+    assert img2.ndim == 2, "Expected single-channel img2."
     h1, w1 = img1.shape[:2]
     h2, w2 = img2.shape[:2]
     vis = np.zeros((max(h1, h2), w1+w2), np.uint8)
@@ -66,13 +91,16 @@ def explore_match(win, img1, img2, kp_pairs, status = None, H = None):
     vis[:h2, w1:w1+w2] = img2
     vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
 
+    # If we found a homography from the object to the
+    # scene, highlight the object in the scene
     if H is not None:
         corners = np.float32([[0, 0], [w1, 0], [w1, h1], [0, h1]])
-        corners = np.int32( cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0) )
-        cv2.polylines(vis, [corners], True, (255, 255, 255))
+        corners = np.int32(cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0))
+        cv2.polylines(vis, [corners], True, (255, 0, 255), thickness=30)
 
     if status is None:
         status = np.ones(len(kp_pairs), np.bool_)
+        
     p1 = np.int32([kpp[0].pt for kpp in kp_pairs])
     p2 = np.int32([kpp[1].pt for kpp in kp_pairs]) + (w1, 0)
 
@@ -80,116 +108,135 @@ def explore_match(win, img1, img2, kp_pairs, status = None, H = None):
     red = (0, 0, 255)
     white = (255, 255, 255)
     kp_color = (51, 103, 236)
+
+    # Highlight keypoints in object and scene
     for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
         if inlier:
             col = green
-            cv2.circle(vis, (x1, y1), 2, col, -1)
+            cv2.circle(vis, (x1, y1), 2, col, thickness=10)
+            cv2.circle(vis, (x2, y2), 2, col, thickness=50)
+        else:
+            col = red
+            r = 8
+            thickness = 20
+            cv2.line(vis, (x1-r, y1-r), (x1+r, y1+r), col, thickness)
+            cv2.line(vis, (x1-r, y1+r), (x1+r, y1-r), col, thickness)
+            cv2.line(vis, (x2-r, y2-r), (x2+r, y2+r), col, thickness)
+            cv2.line(vis, (x2-r, y2+r), (x2+r, y2-r), col, thickness)
+
+    # Connect points in object and scene
+    for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
+        if inlier:
+            cv2.line(vis, (x1, y1), (x2, y2), green, thickness=20)
 
     return vis
 
 # ----------------------------------------------------------------
 
+def to_bgr(img):
+    if img.ndim == 2:
+        return cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    if img.ndim == 3:
+        return img.copy()
+
+# ----------------------------------------------------------------
+
+def foreground_mask(scene_image, empty_scene):
+    '''Create a mask for the foreground pixels in the scene.
+       Works by diffing the scene and the empty scene and
+       building a mask from that.'''
+    # Find the foreground
+    diff = cv2.absdiff(scene_image, empty_scene)
+    ret_val, fg_raw = cv2.threshold(diff, 80, 255, cv2.THRESH_BINARY)
+    # remove small noise...
+    fg_denoise = cv2.erode(fg_raw, kernel=None, iterations=scene_image.shape[0]/1000)
+    # fill the holes...
+    iters = scene_image.shape[0] / 15
+    fg_fat = cv2.dilate(fg_denoise, kernel=None, iterations=iters)
+    # ...and trim down to the contour
+    fg_slim = cv2.erode(fg_fat, kernel=None, iterations=int(.8*iters))
+
+    # Alternatively, we could have computed the convex hull
+    # of the denoised foreground to get a mask
+    # using cv2.findContours and cv2.convexHull
+    
+    return fg_slim
+    
+# ----------------------------------------------------------------
 # For a detailed description of how this works, refer to:
 # http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_feature2d/py_feature_homography/py_feature_homography.html#feature-homography
 
 
 if  __name__ =='__main__':
     image_type = cv2.CV_LOAD_IMAGE_GRAYSCALE
-    pizza_box = cv2.imread('../images/pizza/pizza_box.jpg', image_type)
-    chocolate_box = cv2.imread('../images/pizza/chocolate_box.jpg', image_type)
-    pizza_delivery = read_scene('../images/pizza/pizza_delivery_3.jpg', image_type)
-    chocolate_delivery = read_scene('../images/pizza/chocolate_delivery_1.jpg', image_type)
-
-    # Detection and matching algorithms
-    #detector, norm = cv2.SIFT(), cv2.NORM_L2
-    detector, norm = cv2.ORB(4000), cv2.NORM_HAMMING
+    SAVE_SCREENSHOTS = False
     
-    # Brute-force matcher
-    #matcher = cv2.BFMatcher(norm)
+    # Known object images
+    pizza_box = cv2.imread('../images/pizza/pizza_box_logo_abstract.png', image_type)
+
+    # Scene images
+    empty_scene = cv2.imread('../images/pizza/empty_corridor_1.jpg', image_type)
+    pizza_delivery = cv2.imread('../images/pizza/pizza_delivery_1.jpg', image_type)
+    chocolate_delivery = cv2.imread('../images/pizza/chocolate_delivery_2.jpg', image_type)
+ 
+    # Start the work
+    object_image = pizza_box
+    scene_image = pizza_delivery
+
+    fg_mask = foreground_mask(scene_image, empty_scene)
+
+    #show('Foreground mask', fg_mask)
+    #show('Object', object_image)
+    #show('Scene', scene_image)
+    #show('Scene masked', cv2.min(scene_image, fg_mask))
+
+    
+    # Detection and matching algorithms
+
+    #detector, norm = cv2.SIFT(), cv2.NORM_L2 # SIFT is scale invariant
+    detector, norm = cv2.SURF(), cv2.NORM_L2 # SURF is scale and rotation invariant
+    #detector, norm = cv2.ORB(10000), cv2.NORM_HAMMING
+    
+    bf_matcher = cv2.BFMatcher(norm) # brute-force matcher
     if norm == cv2.NORM_L2:
         FLANN_INDEX_KDTREE = 1
-        flann_matcher = cv2.FlannBasedMatcher(dict(algorithm = FLANN_INDEX_KDTREE, trees = 5), dict(checks = 50))
+        flann_matcher = cv2.FlannBasedMatcher(dict(algorithm = FLANN_INDEX_KDTREE, trees = 5),
+                                              dict(checks = 50))
     else:
         FLANN_INDEX_LSH = 6
-        flann_matcher = cv2.FlannBasedMatcher(dict(algorithm = FLANN_INDEX_LSH, table_number = 6, key_size = 12, multi_probe_level = 1), {})
+        flann_matcher = cv2.FlannBasedMatcher(dict(algorithm = FLANN_INDEX_LSH,
+                                                   table_number = 6, key_size = 12, multi_probe_level = 1), {})
          
-    matcher = flann_matcher
+    matcher = bf_matcher
 
-
-    object_image = chocolate_box
-    scene_image = chocolate_delivery
     
     # Detect features in the reference object
     print "Detecting features..."
     object_keypoints, object_descriptors = detector.detectAndCompute(object_image, None)
     print 'Object has %d features' % len(object_keypoints)
-    scene_keypoints, scene_descriptors = detector.detectAndCompute(scene_image, None)
+    scene_keypoints, scene_descriptors = detector.detectAndCompute(scene_image, fg_mask)
     print 'Scene has %d features' % len(scene_keypoints)
 
-    obj_kp_image = draw_keypoints(cv2.cvtColor(object_image, cv2.COLOR_GRAY2BGR), object_keypoints)
-    scene_kp_image = draw_keypoints(cv2.cvtColor(scene_image, cv2.COLOR_GRAY2BGR), scene_keypoints)
+    obj_kp_image = draw_keypoints(to_bgr(object_image), object_keypoints)
+    scene_kp_image = draw_keypoints(to_bgr(scene_image), scene_keypoints)
     
-    show('Scene', scene_image)
-    show('Object Keypoints', obj_kp_image)
-    show('Scene Keypoints', scene_kp_image)
+    #show('Object Keypoints', obj_kp_image)
+    #show('Scene Keypoints', scene_kp_image)
     
     # match object and scene
     print "Matching..."
     raw_matches = matcher.knnMatch(object_descriptors, scene_descriptors, k=2)
-    #p1, p2, kp_pairs = filter_matches(object_descriptors, scene_descriptors, raw_matches)
-    good = []
-    # store all the good matches as per Lowe's ratio test.
-    for m,n in raw_matches:
-        if m.distance < 0.7*n.distance:
-            good.append(m)
-    print "GOOD points: %d" % len(good)
+    p1, p2, kp_pairs = filter_matches(object_keypoints, scene_keypoints, raw_matches)
 
     MIN_MATCH_COUNT = 10
-
     
-    if len(good)>MIN_MATCH_COUNT:
-        src_pts = np.float32([ object_keypoints[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-        dst_pts = np.float32([ scene_keypoints[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-        matchesMask = mask.ravel().tolist()
-
-        h,w = scene_image.shape
-        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-        dst = cv2.perspectiveTransform(pts,M)
-
-        # Draw the matches
-        match_image = cv2.cvtColor(scene_image, cv2.COLOR_GRAY2BGR)
-        print dst
-        cv2.polylines(match_image,[np.int32(dst)], isClosed = True,
-                      color=(255,255,0), thickness=10, lineType = cv2.CV_AA)
-        cv2.circle(match_image, (100, 100), 100, (255,0,0), thickness=10)
-
-        # draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-        #                     singlePointColor = None,
-        #                     matchesMask = matchesMask, # draw only inliers
-        #                     flags = 2)
-
-        # match_lines_image = cv2.drawMatches(object_image, object_keypoints,
-        #                                     scene_image, scene_keypoints, good, None, **draw_params)
-
-        match_lines_image = explore_match("Title", object_image, scene_image, object_keypoints, scene_keypoints, good)
-
-        #img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10], flags=2)
-
-        show('Matches', match_image)
+    if len(kp_pairs)>MIN_MATCH_COUNT:
+        H, status = cv2.findHomography(p1, p2, cv2.RANSAC, 5.0)
+        print '%d / %d  inliers/matched' % (np.sum(status), len(status))
+        match_lines_image = draw_matches(object_image, scene_image, kp_pairs, status, H)
         show('Match lines', match_lines_image)
-        
     else:
-        print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+        print "Not enough matches are found - %d/%d" % (len(kp_pairs), MIN_MATCH_COUNT)
         matchesMask = None
-        
-    #     H, status = c
-    #     print '%d / %d  inliers/matched' % (np.sum(status), len(status))
-    # else:
-    #     H, status = None, None
-    #     print '%d matches found, not enough for homography estimation' % len(p1)
-    
-    
         
     print "Done."
